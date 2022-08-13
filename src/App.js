a748e7b42a3119d34655063e5fa4789c53fd804d
nahuelell <nahuellell@gmail.com>
2022-08-13 16:07:36 -0300

    primeros archivos React Js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <h1>Hola Mundo</h1>
+    <p>Como les va?</p>
+    </>
+  );
+}
+
+export default App;
